4548d5f4496e3c95df3647162499ed44b223e8f5
Madisyn L <madisyn.lobodzinski@gmail.com>
2025-08-25 14:07:05 -0400

    adding to pizzas

--- python_crash_course/chapter_4/pizzas.py
+++ python_crash_course/chapter_4/pizzas.py
@@ -10,4 +10,19 @@ for pizza in pizzas:
     print(f'{pizza.title()} is a great pizza\n')
 
 #print a statement outside the loop
-print(f'I really love pizza!')
+print(f'I really love pizza!\n')
+
+
+
+#copy list of pizzas
+friend_foods = pizzas[:]
+#add a pizza to the new list
+friend_foods.append('canadian')
+#print both lists to prove they're different
+print(f'My favourite pizzas are:')
+for pizza in pizzas:
+    print(pizza)
+
+print('\nMy friends favourite pizzas are:')
+for pizza in friend_foods:
+    print(pizza)
