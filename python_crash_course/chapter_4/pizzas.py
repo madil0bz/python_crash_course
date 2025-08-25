0590f566864871280af3459474679bde40f4f9b4
Madisyn L <madisyn.lobodzinski@gmail.com>
2025-08-25 13:14:10 -0400

    finished pizza.py

---
+++ python_crash_course/chapter_4/pizzas.py
@@ -0,0 +1,13 @@
+#Generate a list of your favourite pizzas
+pizzas = ['pepperoni', 'cheese', 'hawaiian']
+
+#use for loop to print name of each pizza
+for pizza in pizzas:
+    print(f'{pizza}\n')
+
+#print a sentence for each pizza
+for pizza in pizzas:
+    print(f'{pizza.title()} is a great pizza\n')
+
+#print a statement outside the loop
+print(f'I really love pizza!')
