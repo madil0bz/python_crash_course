aa07f87b07822df210c486e09db108c2359d85ec
Madisyn L <madisyn.lobodzinski@gmail.com>
2025-08-25 13:21:48 -0400

    adding animals

---
+++ python_crash_course/chapter_4/animals.py
@@ -0,0 +1,13 @@
+#List three animals that share characteristics
+animals = ['tiger', 'cat', 'panther']
+
+#print each animal
+for animal in animals:
+    print(f'{animal}\n')
+
+#print sentence for each
+for animal in animals:
+    print(f'A {animal.title()} would make an awesome pet!\n')
+
+#outside of loop statement
+print('All of these animals are great cats!')
